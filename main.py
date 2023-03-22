1bf111f82def43c6ebb8e402ebbb204082a60f9b
Egor <egorko25@gmail.com>
2023-03-22 14:40:59 +0300

    fix

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ if __name__ == "__main__":
     length = int(input("Enter a length of seq: "))
     seq = "1"
 
-    while len(seq) <= length:
+    while len(seq) < length:
         f_min = bma.BerlekampMasseyAlgorithm(bma.Seq2list(sequence=seq))
         X.append(len(seq))
         Y.append(max(f_min))
         seq += rb.randomByte()
-        print(f"Processing: {len(seq)}%", end="\r")
+        print(f"Processing: {int(len(seq) / length * 100)}%", end="\r")
 
+
+
+    print("\nPROCESSED")
     gh.graphCreater(X, Y)
 
 
